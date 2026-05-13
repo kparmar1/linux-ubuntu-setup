601ff39686e285b1f314aa82efb98c2a92a54848
Kal <kparmar1@hotmail.com>
2026-05-13 22:40:32 +0100

    Added Sublime to apps

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -243,6 +243,13 @@ EOF
     sudo add-apt-repository multiverse -y
     sudo apt update -y
     sudo apt install steam-installer -y
+
+    echo "  - Installing Sublime"
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+    echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+    sudo apt-get update -y
+    sudo apt-get install sublime-text -y
+
 }
 
 install_shell() {
